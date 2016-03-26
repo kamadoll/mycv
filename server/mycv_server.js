c75332f068011c14a06da0859cf864e5c360ff80
Shyam Kamadolli <yeahright2001us@yahoo.com>
2016-03-26 13:40:18 -0400

    Added admin functions

--- server/mycv_server.js
+++ server/mycv_server.js
@@ -2,6 +2,11 @@
 
 
   Meteor.startup(function () {
+
+    // Admin functions need access to our user tables
+    Houston.add_collection(Meteor.users);
+    Houston.add_collection(Houston._admins);
+
     // code to run on server at startup
       Modules.server.configureServices();
   });
